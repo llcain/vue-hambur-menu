3cb46fd77eb44776d5ae0c1f1687c4a2c5b9a6c2
LaQuisha Cain <llcain2012@gmail.com>
2025-03-17 20:29:41 -0500

    created vue app

---
+++ hamburgermenu/src/components/TheWelcome.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h2>Hello from Vue!!</h2>
+  </div>
+</template>
+
+<styles>
+</styles>
+ 
+
+ 
---
+++ hamburgermenu/src/components/WelcomeItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>LaQuisha Cain</h1>
+  </div>
+</template>
+
+<style scoped>
+
+
+@media (min-width: 1024px) {
+  
+}
+  
+ 
+ 
+  
+</style>
