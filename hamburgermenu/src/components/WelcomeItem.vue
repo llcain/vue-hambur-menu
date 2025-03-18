8b7211857834924f97df390347fa7b3908a1881f
LaQuisha Cain <llcain2012@gmail.com>
2025-03-17 20:54:30 -0500

    added github steps

--- hamburgermenu/src/components/WelcomeItem.vue
+++ hamburgermenu/src/components/WelcomeItem.vue
@@ -1,6 +1,23 @@
 <template>
   <div>
     <h1>LaQuisha Cain</h1>
+    <h2>Steps to Building a Vue App</h2>
+      <ul>
+        <li>Create Folder in Ubuntu</li>
+        <li>Open in VS code</li>
+          <ol>
+            <li>code .</li>
+          </ol>
+         <li>save to github</li>
+          <ol>
+            <li>git init<li>
+            <li>git add .<li>
+            <li>git commit -m "message"<li>
+            <li>create repo on github<li>
+            <li>git remote add origin "created repo on github link"<li>
+            <li>git push -u origin "branch name"<li>
+          </ol> 
+      </ul>
   </div>
 </template>
 
@@ -8,7 +25,7 @@
 
 
 @media (min-width: 1024px) {
-  
+
 }
   
  
